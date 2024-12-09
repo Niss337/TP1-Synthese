02553ed79ea0b169048daab7b933958f70b73c4b
Niss337 <nissrineelkhatib01@gmail.com>
2024-12-09 08:57:04 +0100

    Update main.c, shell.c, and shell.h after recent modifications

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <string.h>
-#include <stdbool.h>
+#include <stdbool.h>  // Include this for bool type
 #include "shell.h"
 #ifndef CLOCK_MONOTONIC
 #define CLOCK_MONOTONIC 1 // Define a value if not defined
@@ -15,14 +15,13 @@ int main() {
     struct timespec start, end; // Variables for measuring time
 
     disp_welc_mes(); // Display welcome message
-
-    while (1) {  // Infinite loop for REPL (read-eval-print loop)
-     displayPrompt(last_exit_status, is_signal); // Display the prompt
+    while (1) { // Infinite loop for REPL (read-eval-print loop)
+        displayPrompt(last_exit_status, is_signal); // Display the prompt
         if (read_command(command)) { // Read user command
-            write(STDOUT_FILENO, EXIT_PROMPT, strlen(EXIT_PROMPT)); // Use write to output the exit message
+            write(STDOUT_FILENO, EXIT_PROMPT, strlen(EXIT_PROMPT));
             break; // Exit the shell
         }
-        clock_gettime(CLOCK_MONOTONIC, &start); // Start the timer
+         clock_gettime(CLOCK_MONOTONIC, &start); // Start the timer
         execute_command(command, &last_exit_status, &is_signal); // Execute the command
         clock_gettime(CLOCK_MONOTONIC, &end); // Stop the timer
 
