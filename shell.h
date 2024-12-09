a9df3bafe4d23c347082cbc6f3047c65ad44e6e5
Niss337 <nissrineelkhatib01@gmail.com>
2024-12-09 11:58:44 +0100

    Update shell.h Q7

--- shell.h
+++ shell.h
@@ -2,22 +2,25 @@
 #define SHELL_H
 
 #include <stdbool.h>
+#include <unistd.h>  
 
+// Constants for shell messages and prompts
 #define MAX_COMMAND_LENGTH 256
 #define MAX_ARGS 10
 #define WELCOME_MESSAGE "Welcome to the ENSEA Shell.\nType 'exit' to quit.\n"
 #define COMMAND_NOT_FOUND "Command not found.\n"
 #define FORK_FAILED "Fork failed.\n"
-#define EXIT_PROMPT "Bye bye...\n"
-#define PROMPT "enseash"
+#define EXIT_PROMPT "Goodbye...\n"
+#define PROMPT "enseash % "
 #define EXIT_PREFIX "[exit:"
 #define SIGNAL_PREFIX "[sign:"
-#define TIME_SUFFIX "|%dms]%% "
+#define TIME_SUFFIX "|%ldms]%% "
 
 // Function prototypes
 void disp_welc_mes();
 int read_command(char *command);
-void execute_command(char *command, int *last_exit_status, bool *is_signal);
-void displayPrompt(int last_exit_status, bool is_signal);
+void displayPrompt(int last_exit_status, bool is_signal, long exec_time);
+void execute_command(char *command, int *last_exit_status, bool *is_signal, long *exec_time);
+void handle_cd(char *path, int *last_exit_status);
 
 #endif // SHELL_H
