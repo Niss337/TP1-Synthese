4126c8708b97cb9701ead5ab4b35f91e3ef0842a
Niss337 <nissrineelkhatib01@gmail.com>
2024-12-09 10:12:36 +0100

    Update  Q6 main.c

--- main.c
+++ main.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 #include <string.h>
-#include <stdbool.h>
+#include <time.h>
 #include "shell.h"
 
-int main() {
+int main()
+{
     char command[MAX_COMMAND_LENGTH];
-    int last_exit_status = 0;
-    bool is_signal = false;
-
+    int last_exit_status = 0; // Variable to store the last command's exit status
+    bool is_signal = false;// track if the last command was terminated by a signal
     disp_welc_mes();
     while (1) {
         displayPrompt(last_exit_status, is_signal);
         if (read_command(command)) break;
- struct timespec start, end;
-        clock_gettime(CLOCK_MONOTONIC, &start);
+
+        struct timespec start, end;
+        clock_gettime(CLOCK_MONOTONIC, &start); // Start the timer to record when the command execution begins
         execute_command(command, &last_exit_status, &is_signal);
         clock_gettime(CLOCK_MONOTONIC, &end);
-
-        last_exit_status = ((last_exit_status & 0xFF00) | 
-                            ((end.tv_sec - start.tv_sec) * 1000 + 
-                            (end.tv_nsec - start.tv_nsec) / 1000000));
+        // Update the last exit status to include execution time in milliseconds
+         last_exit_status = (last_exit_status & 0xFF00) | 
+                           ((end.tv_sec - start.tv_sec) * 1000 + 
+                            (end.tv_nsec - start.tv_nsec) / 1000000);
     }
     return 0;
 }
